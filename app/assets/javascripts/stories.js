8710e661cbeddd608890720e0dce299bba41b7fa
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-04 15:22:52 -0400

    changes

---
+++ app/assets/javascripts/stories.js
@@ -0,0 +1,24 @@
+//= require angular
+//= require angular-resource
+
+"use strict";
+
+(function(){
+  angular
+  .module("story", ["ngResource"])
+  .controller("story_controller", StoryController);
+
+  StoryController.$inject = ["$resource"];
+
+  function StoryController($resource){
+    var vm= this;
+    var Story = $resource("/stories/:id/json", {}, {
+      vm.data.forEach(function(story){
+      });
+      vm.sort_data_by = function(name){
+        vm.sort_on = name;
+
+      }
+    })
+  }
+})
