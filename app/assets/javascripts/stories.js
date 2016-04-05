b647332f33bad6a4b284a10a3bd190b06ea181a5
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-05 09:42:17 -0400

    story ng-views

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -5,19 +5,50 @@
 
 (function(){
   angular
-  .module("story", ["ngResource"])
-  .controller("story_controller", StoryController);
+  .module("icare", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+  .controller("indexCtrl", [
+    indexCtrlFunction
+  ]);
 
-  StoryController.$inject = ["$resource"];
-
-  function StoryController($resource){
-    var vm= this;
-    var story = $resource("/stories/:id.json", {}, {
-      vm.data.forEach(function(story){
-      });
-      vm.sort_data_by = function(name){
-        vm.sort_on = name;
-      }
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("index", {
+      url: "/",
+      templateUrl: "ng-views/story.index.html"
+      controller: "indexCtrl",
+      controllerAs: "indexVM"
     })
+    .state("show", {
+      url: "/:id"
+    });
   }
-})
+  function indexCtrlFunction(){
+    var indexVM = this;
+    indexVM.foo ='bar';
+  }
+})();
+// (function(){
+//   angular
+//   .module("story", ["ngResource"])
+//   .controller("story_controller", StoryController);
+//
+//   StoryController.$inject = ["$resource"];
+//
+//   function StoryController($resource){
+//     var vm= this;
+//     var story = $resource("/stories/:id.json", {}, {
+//       vm.data.forEach(function(story){
+//       });
+//       vm.sort_data_by = function(name){
+//         vm.sort_on = name;
+//       }
+//     })
+//   }
+// })
